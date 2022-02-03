f61ccda5155319952a64e41832ca48cd97f0cdcc
hangrybear666 <corey.lauster@gmail.com>
2022-02-03 02:16:05 +0100

    Exercises 3.1 - 3.8 full functionality

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
 
 /**
  * express library for rest call handling
@@ -10,26 +12,47 @@ const app = express()
  */
 app.use(express.json())
 
+/**
+ * access control - not providing actual values seems sufficient
+ * allows specific URL to access the server
+ * allows this URL to use specified methods only
+ */
+const corsOptions = {
+  // origin: 'http://localhost:3000',
+  // methods: "GET, PUT, DELETE, POST"
+}
+app.use(cors(corsOptions))
+
+/**
+ * morgan REST logging middleware
+ * the custom token adds logging of added objects for
+ * POST requests and PUT requests, otherwise adds null
+ */
+morgan.token('postBody', function getHeadr (req,res) {
+  return Object.keys(req.body).length === 0 ? null : JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postBody'))
+
 let persons = [
     {
+      "id": 1,
       "name": "Arto Hellas",
-      "number": "040-123456",
-      "id": 1
+      "number": "040-123456"
     },
     {
+      "id": 2,
       "name": "Ada Lovelace",
-      "number": "39-44-5323523",
-      "id": 2
+      "number": "39-44-5323523"
     },
     {
+      "id": 3,
       "name": "Dan Abramov",
-      "number": "12-43-234345",
-      "id": 3
+      "number": "12-43-234345"
     },
     {
+      "id": 4,
       "name": "Mary Poppendieck",
-      "number": "39-23-6423122",
-      "id": 4
+      "number": "39-23-6423122"
     }
   ]
 
@@ -40,6 +63,16 @@ let persons = [
 app.get('/', (request, response) => {
   console.log(`GET server root queried by ${request.rawHeaders[2]} : ${request.rawHeaders[3]}`)
   response.send('<h1>Hello World!</h1>')
+})
+
+/**
+ * http GET request to INFO page of URL
+ * responds with html page
+ */
+ app.get('/info', (request, response) => {
+  console.log(`GET info queried by ${request.rawHeaders[2]} : ${request.rawHeaders[3]}`)
+  response.send(`<p>Phonebook has info for ${persons.length} people.</p> \n
+  ${new Date}`)
 })
 
 /**
@@ -77,17 +110,90 @@ app.delete('/api/persons/:id', (request, response) => {
   const person = persons.find(person => person.id === id)
   if(person) {
     persons = persons.filter(person => person.id !== id )
+    response.status(200).end()
   } else {
-    response.statusMessage = `Person with id ${id} not found.`
-    response.status(204).end()
+    const errorMsg = {error:  `Person with id ${id} not found.`}
+      response.statusMessage = errorMsg.error
+      console.log("ERROR: ", errorMsg.error)
+      response.json(errorMsg)
+      response.status(204)
   }
 })
 
+/**
+ * http POST request to /api/persons/id of URL
+ * responds with nothing, or an error
+ */
 app.post('/api/persons', (request, response) => {
-  console.log("post request")
+  console.log(`POST request received from  ${request.rawHeaders[0]} : ${request.rawHeaders[1]} wanting to add:\n`, request.body)
   const person = request.body
-  console.log(person)
+  const newId = persons.length > 0 ? Math.max(...persons.map(person => person.id +1)) : 0
+  if (!person.name || !person.number) {
+    console.log("ERROR: either name or number missing, no persistence")
+    response.statusMessage = `Object not in correct format`
+    response.status(204).end()
+  } else {
+    const newPerson = {
+      id: newId,
+      name:person.name,
+      number: person.number
+    }
+    const existingPerson = persons.find(person => person.name === newPerson.name)
+    if (existingPerson) {
+      const errorMsg = {error: `Person already in db`}
+      response.statusMessage = errorMsg.error
+      console.log("ERROR: ", errorMsg.error)
+      response.json(errorMsg)
+      response.status(204)
+    } else {
+      persons = persons.concat(newPerson)
+      response.json(newPerson)
+      response.status(200)
+    }
+  }
 })
+
+/**
+ * http PUT request to /api/persons/id of URL
+ * responds with nothing or an error
+ */
+app.put('/api/persons/:id' , (request, response) => {
+  console.log(`PUT request received from  ${request.rawHeaders[0]} : ${request.rawHeaders[1]}
+  > person with id: ${request.params.id} to be updated
+  >> with data:`, request.body)
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if(person) {
+    if (request.body.number) {
+      const updatedPerson = {...person, number: request.body.number}
+      persons = persons.map(person => person.id === id ? updatedPerson :  person)
+      response.json(updatedPerson)
+      response.status(200)
+    } else {
+      const errorMsg = {error: `no number provided in order to update`}
+      response.statusMessage = errorMsg.error
+      console.log("ERROR: ", errorMsg.error)
+      response.json(errorMsg)
+      response.status(204)
+    }
+  } else {
+    const errorMsg = {error: `Person with id ${id} not found.`}
+    response.statusMessage = errorMsg.error
+    console.log("ERROR: ", errorMsg.error)
+    response.json(errorMsg)
+    response.status(204)
+  }
+})
+
+/**
+ * handles all requests not otherwise reflected in the methods of this server
+ * @param {XHR} request
+ * @param {XHR} response
+ */
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
 
 /**
  * Listener keeping the server alive
