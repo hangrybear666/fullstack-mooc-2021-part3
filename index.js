50bea962536f5f4f23ea08716561daf10cfab9c8
hangrybear666 <corey.lauster@gmail.com>
2022-02-03 12:34:22 +0100

    heroku predeployment

--- index.js
+++ index.js
@@ -198,7 +198,7 @@ app.use(unknownEndpoint)
 /**
  * Listener keeping the server alive
  */
-const PORT = 3002
+const PORT = process.env.PORT || 3002
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
